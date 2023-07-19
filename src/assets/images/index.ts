e4120bfe95e9489b99598519da368544bf2659ba
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-07-18 22:35:15 -0300

    ✨ feat(Add Home and Register pages):

---
+++ src/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { default as previewImage } from './app-preview.png'
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { previewImage } from '../../assets'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+import { ClaimUsernameForm } from './components'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
