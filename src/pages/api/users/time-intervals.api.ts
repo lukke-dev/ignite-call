fbe9058f4ab34698ec8522e2b9c949a35aa36bb6
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-07-25 21:53:24 -0300

    ✨ feat(UserTimeIntervals): Criado Rota na api

---
+++ src/pages/api/users/time-intervals.api.ts
@@ -0,0 +1,50 @@
+import { prisma } from '@/lib/prisma'
+import { getServerSession } from 'next-auth'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+interface Interval {
+  weekDay: number
+  startTimeInMinutes: number
+  endTimeInMinutes: number
+}
+
+type timeIntervals = {
+  intervals: Interval[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals }: timeIntervals = req.body
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
+}
